a420c88c4affd6fd62d4d4d7b7d7a3b88915fb55
xiaoticai <xiaoticai@126.com>
2021-03-02 16:54:51 +0800

    框架创建完毕

---
+++ MyProject/MyProject/MyKuangJia/TCPerson.m
@@ -0,0 +1,16 @@
+//
+//  TCPerson.m
+//  MyProject
+//
+//  Created by xiaoticai on 2021/3/2.
+//
+
+#import "TCPerson.h"
+
+@implementation TCPerson
+
+- (void)eat{
+    NSLog(@"%@正在吃。。。。。。",_name);
+}
+
+@end
